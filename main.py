61d080ba29b68432449c5e0cfb51a407795b17ff
Sturm0 <89245373+Sturm0@users.noreply.github.com>
2021-10-08 04:10:33 -0300

    Mejorado main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from time import sleep
 from random import randint,uniform,choice
 from os import system, name
+from random import randint
 
 def limpiar_pantalla():
 	if name == "nt":
@@ -12,7 +13,7 @@ def limpiar_pantalla():
 limpiar_pantalla()
 #Variables globales
 resolución = (1280,720)
-
+listaEnemigo = []
 class naveEspacial(pygame.sprite.Sprite):
 	#clase para las naves
 	def __init__(self):
@@ -42,44 +43,54 @@ class naveEspacial(pygame.sprite.Sprite):
 			if self.rect.top < resolución[1] - resolución[1]/2:
 				self.rect.top = resolución[1] - resolución[1]/2
 	def disparar(self,x,y):
-		print("Disparo")
-		miProyectil = Proyectil(x,y)
+		miProyectil = Proyectil(x,y,"Imagenes/SHOTS.png",True)
 		self.listadisparo.append(miProyectil)
 
 	def dibujar(self,superficie):
 		superficie.blit(self.ImagenNave,self.rect)
 
 class Proyectil(pygame.sprite.Sprite):
-	def __init__(self,posx,posy):
+	def __init__(self,posx,posy, ruta, personaje):
 		pygame.sprite.Sprite.__init__(self)
-		self.imageProyectil = pygame.image.load("Imagenes/SHOTS.png")
+		self.imageProyectil = pygame.image.load(ruta) #"Imagenes/SHOTS.png"
 		self.rect = self.imageProyectil.get_rect()
-		self.velocidadDisparo = 10
+		self.velocidadDisparo = 5
 		self.rect.top = posy
 		self.rect.left = posx
+
+		self.disparoPersonaje = personaje
 	def trayectoria(self):
-		self.rect.top = self.rect.top - self.velocidadDisparo
+		if self.disparoPersonaje == True:
+			self.rect.top = self.rect.top - self.velocidadDisparo
+		else:
+			self.rect.top = self.rect.top + self.velocidadDisparo
 
 	def dibujar(self,superficie):
 		superficie.blit(self.imageProyectil,self.rect)
 
 class Invasor(pygame.sprite.Sprite):
-	def __init__(self,posx,posy):
+	def __init__(self,posx,posy,distancia, ImagenUno, ImagenDos, ImagenTres):
 		pygame.sprite.Sprite.__init__(self)
-		self.imagenA = pygame.image.load("Imagenes/enemigos/ENEMY01.png")
-		self.imagenB = pygame.image.load("Imagenes/enemigos/ENEMY02.png")
-		self.imagenC = pygame.image.load("Imagenes/enemigos/ENEMY03.png")
-
+		self.imagenA = pygame.image.load(ImagenUno)
+		self.imagenB = pygame.image.load(ImagenDos)
+		self.imagenC = pygame.image.load(ImagenTres)
 		self.listaimagenes = [self.imagenA,self.imagenB,self.imagenC]
 		self.posImagen = 0
 		self.imagenInvasor = self.listaimagenes[self.posImagen]
 		self.rect = self.imagenInvasor.get_rect()
 		self.listadisparo = []
-		self.velocidad = 20
+		self.velocidad = 2
 		self.rect.top = posy
 		self.rect.left = posx
+		self.rangoDisparo = 1 #Determina la probabilidad de disparo
+		self.tiempoCambio = 3
 
-		self.tiempoCambio = 1
+		self.derecha = True
+		self.contador = 0
+		self.Maxdescenso = self.rect.top + 40
+
+		self.limiteDerecha = posx + distancia
+		self.limiteIzquierda = posx #- distancia
 	def trayectoria(self):
 		self.rect.top = self.rect.top - self.velocidadDisparo
 
@@ -88,6 +99,10 @@ class Invasor(pygame.sprite.Sprite):
 		superficie.blit(self.imagenInvasor,self.rect)
 
 	def comportamiento(self,tiempo):
+		#algoritmo de comportamiento
+		self.__movimientos()
+		self.__ataque()
+		print(self.tiempoCambio,tiempo)
 		if self.tiempoCambio == round(tiempo):
 			self.posImagen += 1
 			self.tiempoCambio += 1
@@ -95,15 +110,68 @@ class Invasor(pygame.sprite.Sprite):
 			if self.posImagen > len(self.listaimagenes)-1:
 				self.posImagen = 0
 
+	def __ataque(self):
+		número_random_temporal = randint(1,450)
+		#print(número_random_temporal)
+		if número_random_temporal == self.rangoDisparo:
+			self.__disparo()
+	def __disparo(self):
+		x,y = self.rect.center
+		miProyectil = Proyectil(x,y,"Imagenes/enemigos/ESHOT_0.png", False)
+		self.listadisparo.append(miProyectil)
+
+	def __movimientos(self):
+		self.__movimientoLateral()
+
+		#Bloque todavía no implementado
+		# if self.contador < 3:
+		# 	self.__movimientoLateral()
+		# else:
+		# 	self.__descenso()
+
+	# def __descenso(self):
+	# 	if self.Maxdescenso == self.rect.top:
+	# 		self.contador = 0
+	# 		self.Maxdescenso = self.rect.top + 40
+	# 	else:
+	# 		self.rect.top += 1
+
+	def __movimientoLateral(self):
+		if self.derecha == True:
+			self.rect.left = self.rect.left + self.velocidad
+			if self.rect.left > self.limiteDerecha:
+				self.derecha = False
+				#self.contador += 1
+		else:
+			self.rect.left = self.rect.left - self.velocidad
+			if self.rect.left < self.limiteIzquierda:
+				self.derecha = True
+				#self.contador += 1
+
+def cargarEnemigos():
+
+	
+	lista_y = [20,120,220]
+	posx = 100
+	for cada_uno in lista_y:
+		for x in range(1,10):
+			enemigo = Invasor(posx,cada_uno,300,"Imagenes/enemigos/ENEMY01.png","Imagenes/enemigos/ENEMY02.png","Imagenes/enemigos/ENEMY03.png")
+			listaEnemigo.append(enemigo)
+			posx += 75
+			if x == 9:
+				posx = 100
+				
 def InvasionEspacial():
 	pygame.init()
-	#sonido = pygame.mixer.Sound("sonido.wav")
-	#sonido.set_volume(0.5)
+	sonido = pygame.mixer.Sound("./Sonidos/space.ogg")
+	#sonido.play()
+	
 	#miFuente = pygame.font.Font('.ttf',20)
 	ventana = pygame.display.set_mode(resolución) #,flags=pygame.FULLSCREEN|pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.SCALED
 	#ImagenFondo = pygame.image.load('Imagenes/Fondo.jpg')
 	jugador = naveEspacial()
-	enemigo = Invasor(100,100)
+	cargarEnemigos()
+
 	enJuego = True
 	acumulador = 0
 	reloj = pygame.time.Clock()
@@ -178,19 +246,27 @@ def InvasionEspacial():
 		
 		#demoProyectil.dibujar(ventana)
 		#ventana.blit(ImagenFondo, (0,0))
+		if len(listaEnemigo) > 0:
+			for enemigo in listaEnemigo:
+				enemigo.comportamiento(tiempo)
+				enemigo.dibujar(ventana)
+
+				if len(enemigo.listadisparo) > 0:
+					for x in enemigo.listadisparo:
+						x.dibujar(ventana)
+						x.trayectoria()
+						if x.rect.top > resolución[1]:
+							enemigo.listadisparo.remove(x)
+
 		if len(jugador.listadisparo) > 0:
 			for x in jugador.listadisparo:
 				x.dibujar(ventana)
 				x.trayectoria()
 				if x.rect.top < 0: #para que se elimnen las balas cuando no están en la ventana
 					jugador.listadisparo.remove(x)
-		enemigo.comportamiento(tiempo)			
-		jugador.dibujar(ventana)
-		enemigo.dibujar(ventana)
 
-		#sleep(1/60)
-		#pygame.time.wait(round((1/60)*1000))
+		jugador.dibujar(ventana)
 		reloj.tick(60)
 		pygame.display.update()
-		print(f"{reloj.get_fps():.2f}")
+		#print(f"{reloj.get_fps():.2f}")
 InvasionEspacial()
